7ab99b3f28241f87376dce43121f1e674347273c
wiop93256 <155625295+wiop93256@users.noreply.github.com>
2024-12-13 14:05:23 +0800

    Optimize: 优化Java的缩进为4个空格。

--- formatter/Java/java-formatter.mjs
+++ formatter/Java/java-formatter.mjs
@@ -5,6 +5,8 @@ import prettierJavaParser from "prettier-plugin-java";
 export default async function formatCode(code) {
   return prettier.format(code, {
     parser: "java",
+    tabWidth: 4,
+    useTabs: false,
     plugins: [prettierHtmlParser, prettierJavaParser],
   });
 }
